9bcc8b7bfceab556742aabc3dff3eb35813abf91
drewkwan <andrewtimkwan@gmail.com>
2022-08-12 09:12:07 +0800

    first commit

---
+++ src/test/java/vttp/ssfAssessment/ssfAssessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.ssfAssessment.ssfAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
